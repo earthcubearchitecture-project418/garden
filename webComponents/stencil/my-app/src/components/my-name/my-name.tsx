176aefd7f334276331d35bcf958571ba8afe38c9
Douglas Fils <dfils@oceanleadership.org>
2017-10-28 11:51:52 -0500

    testing d3 and json-ld viz libraries in stencil web components

--- webComponents/stencil/my-app/src/components/my-name/my-name.tsx
+++ webComponents/stencil/my-app/src/components/my-name/my-name.tsx
@@ -1,7 +1,6 @@
 import { Component, Prop } from '@stencil/core';
 // import jsonld from 'jsonld';
 
-
 // Notes:
 // Look at:
 // https://stackoverflow.com/questions/41408471/parse-json-array-in-typescript
@@ -9,33 +8,6 @@ import { Component, Prop } from '@stencil/core';
 // maybe if it is flattened.  Also the JSON from Bleve call will likely 
 // just be JSON, 
 
-// declare module namespace {
-  
-//       export interface IcalDtstart {
-//           @type: string;
-//       }
-  
-//       export interface Context {
-//           ical: string;
-//           xsd: string;
-//           ical:dtstart: IcalDtstart;
-//       }
-  
-//       export interface RootObject {
-//           @context: Context;
-//           ical:summary: string;
-//           ical:location: string;
-//           ical:dtstart: string;
-//       }
-  
-//   }
-   
-
-// class Person {
-//   constructor(public name: string, 
-//               public surname: string, 
-//         public age: number){}
-// }
 
 @Component({
   tag: 'my-name',
@@ -43,52 +15,43 @@ import { Component, Prop } from '@stencil/core';
 })
 export class MyName {
 
-
   @Prop() first: string;
   @Prop() last: string;
 
+
+
   render() {
 
-  
+  var e = document.getElementById('test');
+  var jsonld = e.textContent;
+  // need to take JSONLD and try and parse it
 
-    // let mark = new Person('Mark', 'Galea', 30);
-    // let mark = new namespace();
-    
-    // interface Test {
-    //   icalSummary: string;
-    // }
+  interface MyObj {
+    id: string
+    type: string
+  }
 
-    // jsonld.compact(doc, context, function(err, compacted) {
-      // console.log(JSON.stringify(compacted, null, 2));
-      /* Output:
-      {
-        "@context": {...},
-        "name": "Manu Sporny",
-        "homepage": "http://manu.sporny.org/",
-        "image": "http://manu.sporny.org/images/manu.png"
-      }
-      */
-    // });
-
-    var e = document.getElementById('test');
-    var jsonld = e.textContent;
-    // need to take JSONLD and try and parse it
+  let obj: { string: MyObj[] } = JSON.parse(jsonld.toString());
 
 
-    interface MyObj {
-      id: string
-      type: string
-    }
-    
-    let obj: { string: MyObj[] } = JSON.parse(jsonld.toString());
+  var element = obj["@graph"]
+  console.log(element[0]["schema:contentUrl"])
+  var url = element[0]["schema:contentUrl"]
 
-    // console.log(obj.string[0].id, obj.string[0].type);
-    console.log(obj["@graph"]);
-    
-    
+
     return (
-      <div>
-        Hello There, my name is <br/><br/>{this.first} {this.last}  <br/><br/>{jsonld}  <br/><br/>
+      <div class="earthcube-provinfo">
+        Hello There, my name is {this.first} {this.last}
+        <div>
+        <a href={url} target="_blank">{url}</a>
+        </div>
+       <details>
+         <summary>
+           More information
+           </summary>
+           <a href={url} target="_blank">{url}</a>
+         </details>
+
       </div>
     );
   }
