37a99c8d2d0963d64056603b18000c880783f9e6
Adam Shepherd <ashepherd@whoi.edu>
2018-04-07 10:51:35 -0400

    uses WITHIN; look for FeatureCollection
    WITHIN will grab all the POINTS inside the geom while INTERSECT would only return POINTS that fall on the line (if i understood the tile38 docs)

--- spatialtesting/simpleClient/simpleclient.go
+++ spatialtesting/simpleClient/simpleclient.go
@@ -1,216 +1,233 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"strings"
+  "encoding/json"
+  "fmt"
+  "log"
+  "strings"
 
-	"github.com/garyburd/redigo/redis"
-	geojson "github.com/paulmach/go.geojson"
+  "github.com/garyburd/redigo/redis"
+  geojson "github.com/paulmach/go.geojson"
 )
 
 type LocType struct {
-	Type string `json:"type"`
+  Type string `json:"type"`
 }
 
 func main() {
-	fmt.Println("this is a simple spatial test")
+  fmt.Println("this is a simple spatial test")
 
-	SpatialCall(testgeom(), "")
+  SpatialCall(testgeom(), "")
 
 }
 
 func testgeom() string {
 
-	data := `{
-	"type": "FeatureCollection",
-	"features": [
-	  {
-		"type": "Feature",
-		"properties": {},
-		"geometry": {
-		  "type": "Polygon",
-		  "coordinates": [
-			[
-			  [
-				-112.8515625,
-				-29.535229562948444
-			  ],
-			  [
-				85.4296875,
-				-29.535229562948444
-			  ],
-			  [
-				85.4296875,
-				65.36683689226321
-			  ],
-			  [
-				-112.8515625,
-				65.36683689226321
-			  ],
-			  [
-				-112.8515625,
-				-29.535229562948444
-			  ]
-			]
-		  ]
-		}
-	  }
-	]
+  data := `{
+  "type": "FeatureCollection",
+  "features": [
+    {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+      "type": "Polygon",
+      "coordinates": [
+      [
+        [
+        -112.8515625,
+        -29.535229562948444
+        ],
+        [
+        85.4296875,
+        -29.535229562948444
+        ],
+        [
+        85.4296875,
+        65.36683689226321
+        ],
+        [
+        -112.8515625,
+        65.36683689226321
+        ],
+        [
+        -112.8515625,
+        -29.535229562948444
+        ]
+      ]
+      ]
+    }
+    }
+  ]
   }
   `
 
-	return data
+  return data
 }
 
 func redisDial() (redis.Conn, error) {
-	// c, err := redis.Dial("tcp", "tile38:9851")
-	c, err := redis.Dial("tcp", "localhost:9851")
-	if err != nil {
-		log.Printf("Could not connect: %v\n", err)
-	}
-	return c, err
+  // c, err := redis.Dial("tcp", "tile38:9851")
+  c, err := redis.Dial("tcp", "localhost:9851")
+  if err != nil {
+    log.Printf("Could not connect: %v\n", err)
+  }
+  return c, err
 }
 
 func SpatialCall(geowithin, filter string) {
-	// geowithin := request.QueryParameter("geowithin")
-	// filter := request.QueryParameter("filter")
-	// log.Printf("Called with filter: %s and geojson %s \n", filter, geowithin)
+  // geowithin := request.QueryParameter("geowithin")
+  // filter := request.QueryParameter("filter")
+  // log.Printf("Called with filter: %s and geojson %s \n", filter, geowithin)
 
-	_, err := geojson.UnmarshalFeatureCollection([]byte(geowithin))
-	if err != nil {
-		fmt.Println(err)
-	}
+  _, err := geojson.UnmarshalFeatureCollection([]byte(geowithin))
+  if err != nil {
+    fmt.Println(err)
+  }
 
-	c, err := redisDial()
-	defer c.Close()
+  c, err := redisDial()
+  defer c.Close()
 
-	var value1 int
-	var value2 []interface{}
-	// TODO  fix the 50K request limit, put in cursor pattern
-	reply, err := redis.Values(c.Do("INTERSECTS", "p418", "LIMIT", "50000", "OBJECT", geowithin))
-	// reply, err := redis.Values(c.Do("SCAN", "p418"))  // an early test call just to get everything
-	if err != nil {
-		fmt.Printf("Error in reply %v \n", err)
-	}
-	if _, err := redis.Scan(reply, &value1, &value2); err != nil {
-		fmt.Printf("Error in scan %v \n", err)
-	}
+  var value1 int
+  var value2 []interface{}
+  // TODO  fix the 50K request limit, put in cursor pattern
+  reply, err := redis.Values(c.Do("WITHIN", "p418", "LIMIT", "50000", "OBJECT", geowithin))
+  //WITHIN p418 BOUNDS -90 -40 17 50
+  // reply, err := redis.Values(c.Do("WITHIN", "p418", "BOUNDS", -90, -40, 17, 50))
+  // reply, err := redis.Values(c.Do("SCAN", "p418"))  // an early test call just to get everything
+  if err != nil {
+    fmt.Printf("Error in reply %v \n", err)
+  }
+  if _, err := redis.Scan(reply, &value1, &value2); err != nil {
+    fmt.Printf("Error in scan %v \n", err)
+  }
 
-	// log.Println(value1) // the point of this logging is what?  the point of value1 is what!?
+  // log.Println(value1) // the point of this logging is what?  the point of value1 is what!?
+  // log.Println(value2)
 
-	results, err := redisToGeoJSON(value2, filter)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(results)
+  results, err := redisToGeoJSON(value2, filter)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(results)
 }
 
 func redisStringToGeoJSON(m map[string]string) (string, error) {
 
-	fc := geojson.NewFeatureCollection()
+  fc := geojson.NewFeatureCollection()
 
-	for k, v := range m {
-		fmt.Println("k:", k, "v:", v)
+  for k, v := range m {
+    fmt.Println("k:", k, "v:", v)
 
-		rawGeometryJSON := []byte(v)
-		ID := k
+    rawGeometryJSON := []byte(v)
+    ID := k
 
-		g, err := geojson.UnmarshalGeometry(rawGeometryJSON)
-		if err != nil {
-			log.Printf("Unmarshal geom error for %s with %s\n", rawGeometryJSON, err)
-		}
+    g, err := geojson.UnmarshalGeometry(rawGeometryJSON)
+    if err != nil {
+      log.Printf("Unmarshal geom error for %s with %s\n", rawGeometryJSON, err)
+    }
 
-		switch {
-		case g.IsPoint():
-			nf := geojson.NewFeature(g)
-			nf.SetProperty("URL", ID)
-			fc.AddFeature(nf)
-		case g.IsPolygon():
-			nf := geojson.NewFeature(g)
-			nf.SetProperty("URL", ID)
-			fc.AddFeature(nf)
-		default:
-			log.Println(g.Type)
-		}
+    switch {
+    case g.IsPoint():
+      nf := geojson.NewFeature(g)
+      nf.SetProperty("URL", ID)
+      fc.AddFeature(nf)
+    case g.IsPolygon():
+      nf := geojson.NewFeature(g)
+      nf.SetProperty("URL", ID)
+      fc.AddFeature(nf)
+    default:
+      log.Println(g.Type)
+    }
 
-		if g.Type == "Feature" {
-			f, err := geojson.UnmarshalFeature(rawGeometryJSON)
-			if err != nil {
-				log.Printf("Unmarshal feature error for %s with %s\n", ID, err)
-			}
-			f.SetProperty("URL", ID)
-			fc.AddFeature(f)
-		}
+    if g.Type == "Feature" {
+      f, err := geojson.UnmarshalFeature(rawGeometryJSON)
+      if err != nil {
+        log.Printf("Unmarshal feature error for %s with %s\n", ID, err)
+      }
+      f.SetProperty("URL", ID)
+      fc.AddFeature(f)
+    }
 
-	}
+  }
 
-	rawJSON, err := fc.MarshalJSON()
-	if err != nil {
-		return "", err
-	}
+  rawJSON, err := fc.MarshalJSON()
+  if err != nil {
+    return "", err
+  }
 
-	return string(rawJSON), nil
+  return string(rawJSON), nil
 }
 
 func redisToGeoJSON(results []interface{}, filter string) (string, error) {
 
-	fc := geojson.NewFeatureCollection()
+  fc := geojson.NewFeatureCollection()
 
-	for _, item := range results {
-		valcast := item.([]interface{})
-		val0 := fmt.Sprintf("%s", valcast[0])
-		val1 := fmt.Sprintf("%s", valcast[1])
-		// log.Printf("%s %s \n", val0, val1)
+  for _, item := range results {
+    valcast := item.([]interface{})
+    val0 := fmt.Sprintf("%s", valcast[0])
+    val1 := fmt.Sprintf("%s", valcast[1])
+    //log.Printf("%s %s \n", val0, val1)
 
-		if strings.Contains(val0, filter) || filter == "" {
+    if strings.Contains(val0, filter) || filter == "" {
 
-			lt := &LocType{}
-			err := json.Unmarshal([]byte(val1), lt)
-			if err != nil {
-				log.Print(err)
-				return "", err
-			}
+      lt := &LocType{}
+      err := json.Unmarshal([]byte(val1), lt)
+      if err != nil {
+        log.Print(err)
+        return "", err
+      }
 
-			rawGeometryJSON := []byte(val1)
+      rawGeometryJSON := []byte(val1)
 
-			if lt.Type == "Feature" {
-				f, err := geojson.UnmarshalFeature(rawGeometryJSON)
-				if err != nil {
-					log.Printf("Unmarshal feature error for %s with %s\n", val0, err)
-				}
-				f.SetProperty("URL", val0)
-				fc.AddFeature(f)
-			}
+      switch (lt.Type) {
+        case "FeatureCollection":
+          fcf, err := geojson.UnmarshalFeatureCollection(rawGeometryJSON)
+          if err != nil {
+            log.Printf("Unmarshal featurecollection error for %s with %s\n", val0, err)
+          }
+          for _, f := range fcf.Features {
+            f.SetProperty("URL", val0)
+            fc.AddFeature(f)
+          }
+          break
 
-			if lt.Type == "Point" || lt.Type == "Poly" {
-				g, err := geojson.UnmarshalGeometry(rawGeometryJSON)
-				if err != nil {
-					log.Printf("Unmarshal geom error for %s with %s\n", val0, err)
-				}
+        case "Feature":
+          f, err := geojson.UnmarshalFeature(rawGeometryJSON)
+          if err != nil {
+            log.Printf("Unmarshal feature error for %s with %s\n", val0, err)
+          }
+          f.SetProperty("URL", val0)
+          fc.AddFeature(f)
+          break
 
-				switch {
-				case g.IsPoint():
-					nf := geojson.NewFeature(g)
-					nf.SetProperty("URL", val0)
-					fc.AddFeature(nf)
-				case g.IsPolygon():
-					nf := geojson.NewFeature(g)
-					nf.SetProperty("URL", val0)
-					fc.AddFeature(nf)
-				default:
-					log.Println(g.Type)
-				}
-			}
-		}
-	}
+        case "Point":
+        case "Poly":
+          g, err := geojson.UnmarshalGeometry(rawGeometryJSON)
+          if err != nil {
+            log.Printf("Unmarshal geom error for %s with %s\n", val0, err)
+          }
 
-	rawJSON, err := fc.MarshalJSON()
-	if err != nil {
-		return "", err
-	}
+          switch {
+            case g.IsPoint():
+              nf := geojson.NewFeature(g)
+              nf.SetProperty("URL", val0)
+              fc.AddFeature(nf)
+            case g.IsPolygon():
+              nf := geojson.NewFeature(g)
+              nf.SetProperty("URL", val0)
+              fc.AddFeature(nf)
+            default:
+              log.Println(g.Type)
+          }
+          break
+      }
+    }
+  }
 
-	return string(rawJSON), nil
+  rawJSON, err := fc.MarshalJSON()
+  if err != nil {
+    return "", err
+  }
+
+  return string(rawJSON), nil
 }
